0b94b898aebb9504897811a91d527f949e821f9f
ThomasAnkcorn <thomasankcorn@gmail.com>
2020-04-10 23:58:05 +0100

    tailwind config complete

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -2,7 +2,11 @@ const rules = require('./webpack.rules');
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  use: [
+    'style-loader',
+    { loader: 'css-loader', options: { importLoaders: 1 } },
+    'postcss-loader'
+]
 });
 
 module.exports = {
