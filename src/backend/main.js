b0dd28f89b4331faa7fa17bd8e50bbd332ea977d
ThomasAnkcorn <thomasankcorn@gmail.com>
2020-04-12 20:01:03 +0100

    making a mess

--- src/backend/main.js
+++ src/backend/main.js
@@ -1,46 +1,60 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray } = require('electron');
 const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
+app.dock.hide();
+let window, tray;
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  window = new BrowserWindow({
+      width: 320,
+      height: 450,
+      show: false,
+      frame: false,
+      fullscreenable: false,
+      resizable: false,
+      transparent: true,
   });
+  window.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+  // Hide the window when it loses focus
+  window.on('blur', () => {
+      if (!window.webContents.isDevToolsOpened()) {
+          window.hide();
+      }
+  });
+}
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+const getWindowPosition = () => {
+  const windowBounds = window.getBounds();
+  const trayBounds = tray.getBounds();
+  
+  // Center window horizontally below the tray icon
+  const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
+  // Position window 4 pixels vertically below the tray icon
+  const y = Math.round(trayBounds.y + trayBounds.height + 4)
+  return {x: x, y: y}
+}
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
+const showWindow = () => {
+  const position = getWindowPosition();
+  window.setPosition(position.x, position.y, false);
+  window.show();
+}
 
+const toggleWindow = () => {
+  window.isVisible() ? window.hide() : showWindow();
+}
+
+const createTray = () => {
+  tray = new Tray(path.join(__dirname, 'Group@2x.png'));
+  tray.on('click', function (event) {
+    toggleWindow();
+})
+}
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on('ready', () => {
+  createTray();
+  createWindow();
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
