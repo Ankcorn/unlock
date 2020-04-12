64a6d2d5ae7c3a62632a03a030628905b0a061c5
ThomasAnkcorn <thomasankcorn@gmail.com>
2020-04-12 23:59:48 +0100

    lit lit lit 💨 🌴

--- src/app/jwt.js
+++ src/app/jwt.js
@@ -1,23 +1,21 @@
-export default class JWTView {
-  constructor() {
-    const body = document.querySelector('body')
-    body.innerHTML = `
-    <div class="fixed h-48 bg-gray-800" />
-    <div class="flex flex-col h-screen justify-between">
-    <navigation class="px-6 pt-2 flex flex-row items-center w-screen">
-      <button href="#">
-        <svg viewBox="0 0 20 20" class="w-8 h-8 text-white fill-current">
-          <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-        </svg>
-      </button>
-      <h1 class="mx-6 font-bold text-xl text-gray-100">
-        Google @ NearSt
-      </h1>
-    </navigation>
-    <main class="bg-gray-100 h-full mx-6 my-2 rounded-lg p-4">
-    </main>
-    <footer class="px-6 py-3">
-      <button class="bg-blue-500 w-full p-2 rounded-md text-white">Copy JWT</button>
-    </footer>`
-  }
-}
+import { html } from 'lit-html'
+
+export default html`
+  <div class="fixed h-48 bg-gray-800" />
+  <div class="flex flex-col h-screen justify-between">
+  <navigation class="px-6 pt-2 flex flex-row items-center w-screen">
+    <button href="#">
+      <svg viewBox="0 0 20 20" class="w-8 h-8 text-white fill-current">
+        <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+      </svg>
+    </button>
+    <h1 class="mx-6 font-bold text-xl text-gray-100">
+      Google @ NearSt
+    </h1>
+  </navigation>
+  <main class="bg-gray-100 h-full mx-6 my-2 rounded-lg p-4">
+  </main>
+  <footer class="px-6 py-3">
+    <button class="bg-blue-500 w-full p-2 rounded-md text-white">Copy JWT</button>
+  </footer>
+`
--- src/app/renderer.js
+++ src/app/renderer.js
@@ -1,8 +1,33 @@
 import './index.css';
-import Jwt from './jwt';
-import Home from './home'
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+import { render } from 'lit-html'
 
-const data = ['', '']
-new Home(data);
+// import Jwt from './jwt';
+import home from './home'
+// console.log('👋 This message is being logged by "renderer.js", included via webpack');
+
+function onFooterClick() {
+  console.log('hi')
+  data.push('test')
+}
+
+const state = {
+  page: 'home',
+  homeData: ['Google @ NearST', 'Shop-Owners', 'Testing'],
+  jwtData: {}
+}
+// new Home(data);
 // new Jwt();
+function build(state) {
+ 
+
+  switch(state.page) {
+    case 'home':
+      return render(home({ onFooterClick, data: state.homeData }), document.body);
+    case 'jwt':
+      return render(jwt(), document.body);
+    default:
+      return render(home({ onFooterClick, data: state.homeData }), document.body);
+  }
+}
+
+build(state);
